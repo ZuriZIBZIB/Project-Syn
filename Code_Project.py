d22a643d911ecb9aa5b06a043bd4aaf24e1bb24c
Zuri <meizafirza@gmail.com>
2025-05-24 20:01:40 +0700

    Bener nih

--- Code_Project.py
+++ Code_Project.py
@@ -1,3 +1,26 @@
-angka1 = int(input("Angka 1 : ")
-operator = int(input("Pilih  Operasi : ")
-angka2 = int(input("Angka 2 : ")
+print("\n===Kalkulator Sederhana===\n")
+
+angka1 = int(input("Angka 1 : "))
+operator = (input("Pilih  Operasi : "))
+angka2 = int(input("Angka 2 : "))
+
+if operator == "+" :
+    hasil = (angka1 + angka2)
+    print("Hasilnya adalahh : ",hasil)
+
+elif operator == "-" :
+    hasil = (angka1 - angka2)
+    print("Hasilnya adalahh : ",hasil)
+
+elif operator == "*" or operator == "x" :
+    hasil = (angka1 * angka2)
+    print("Hasilnya adalahh : ",hasil)
+
+elif operator == "/" :
+    hasil = (angka1 / angka2)
+    print("Hasilnya adalahh : ",hasil)
+
+else :
+    print("Pastikan Anda Input Dengan Benar")
+
+print("\nThis Is End Of Program\n")
